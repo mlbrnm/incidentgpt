6eb570d00aa3f3fb4af1ae3426b31d809e69c271
mlbrnm <mlbrnm@github.io>
2024-10-30 15:41:56 -0600

    Update credential template.

--- credentialsTEMPLATE.py
+++ credentialsTEMPLATE.py
@@ -4,3 +4,5 @@ servicenow_instance = 'https://yourinstance.service-now.com'
 user = 'apiusername' 
 password = 'password/token' 
 endpoint = f'{servicenow_instance}/api/now/table/incident'
+ZABBIXURL = "https://website.com/zabbix/api_jsonrpc.php"
+ZABBIXTOKEN = "yourtoken"
